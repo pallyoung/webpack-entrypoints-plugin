0466a3d407d99c30b43fbf174ddc2e60d8c9d838
shyeshaopeng <yeshaopeng@corp.netease.com>
2018-09-10 17:55:04 +0800

    v1.0.0

---
+++ index.js
@@ -0,0 +1,43 @@
+const name = 'WebpackEntrypointsPlugin'
+const fs = require('fs');
+class WebpackEntrypointsPlugin {
+  constructor(options) {
+    this.path = options.path;
+  }
+  apply(compiler) {
+    compiler.hooks.done.tap(name,  (stats) =>{
+      const chunkOnlyConfig = {
+        assets: false,
+        cached: false,
+        children: true,
+        chunks: true,
+        chunkModules: false,
+        chunkOrigins: false,
+        errorDetails: false,
+        hash: false,
+        modules: false,
+        reasons: false,
+        source: false,
+        timings: false,
+        version: false
+      };
+      const entrypoints = stats.toJson(chunkOnlyConfig).entrypoints;
+      const path = this.path;
+      let data = {};
+
+      if (fs.existsSync(path)) {
+        data = fs.readFileSync(path).toJSON();
+
+      }
+      for (let en in entrypoints) {
+        data[en] = {
+          chunks: entrypoints[en].chunks,
+          assets: entrypoints[en].assets,
+        }
+      }
+      fs.writeFileSync(path,JSON.stringify(data,null,'\t'));
+    })
+  }
+}
+
+module.exports = WebpackEntrypointsPlugin;
